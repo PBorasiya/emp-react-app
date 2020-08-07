b328b94ac1ee56097323399643ead759a7f113ce
PBorasiya <parixit.borasia@gmail.com>
2020-08-07 11:55:39 -0700

    Created first component

---
+++ src/components/EmployeeComponent.jsx
@@ -0,0 +1,14 @@
+import React ,{ Component } from 'react'
+
+
+class EmployeeComponent extends Component{
+    render(){
+        return (
+            <div>
+                First Component
+            </div>
+        )
+    }
+}
+
+export default EmployeeComponent
